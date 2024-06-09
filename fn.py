5d81aa4532d11207cd0ad9b1b8943fbcbad4cc1e
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-09 23:49:08 +0530

    Create fn.py

---
+++ fn.py
@@ -0,0 +1,54 @@
+#functions = a block of code which is executed only when it is clled!!
+
+#def hello():
+#    print("hello!")
+#    print("have a nice day!")
+#hello()
+
+#def hello(first_name,last_name,age):
+#    print("hello "+first_name+" "+last_name)
+#    print("your age is "+str(age)+" years old")
+#     print("have a nice day!")
+#hello("xiang","tzu",25)
+#hello("jack")
+
+#****an important feature of functions
+# is we can send information to our function(it could be str,variable,collection.....etc anything!!) ****
+#                                  the information we pass is called an **argument**
+#**another way of writting code**
+#def hello(first_name,last_name,age):
+ #   full_name = first_name + last_name
+#    print("hello "+full_name)
+#    age = str(age)
+#    print("your age is "+age+" years old")
+#    print("have a nice day")
+#hello("xiang","tzu",25)
+
+#return statement = a statement used within a function to send python value/object back to the caller!!**
+#                    ***and these value/objects known as function's return value!****
+
+#def multiply(number1,number2):
+#    result = number1 * number2
+#    return result
+#x = multiply(6,7)
+#y = multiply(3.8,4)
+#print(x)
+#print(y)
+
+#def multiply(a,b):
+#    r = a * b
+
+#x = multiply(2,4)
+#print(x)
+
+#def multiply(number1,number2):
+#    result = number1*number2
+#    return result
+
+#x = multiply(6,7)
+#print(x)
+
+#def multiply(a,b): #way of writting code in less lines!!
+#    return a*b
+#x = multiply(3,4)
+#print(x)
